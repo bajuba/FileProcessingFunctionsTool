752bd194f755f57d062cb586c107f55ad700d4ec
Drew Stoutin <drewstoutin97@gmail.com>
2022-01-31 14:09:27 -0600

    Add removeEveryOtherChar() to tools.py

--- tools.py
+++ tools.py
@@ -1,1 +1,16 @@
+def removeEveryOtherChar():
+    f = open("a")
+    content = f.read()
+    i = 0
+    contentUpdated = ""
+    for char in content:
+        if char.isalpha() or char.isnumeric():
+            if (i % 2) == 0:
+                contentUpdated += char
+            i += 1 
+        else:
+            contentUpdated += char
+    print(contentUpdated)
 
+
+removeEveryOtherChar()
